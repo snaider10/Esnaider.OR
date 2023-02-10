a4b267f12c6114a07fbbe2e575976bb95f658e8b
Darkin <snaiderortega10@gmail.com>
2023-02-09 23:05:59 -0500

    Agregando Primeros Archivos

---
+++ src/data/contactos.js
@@ -0,0 +1,17 @@
+//array con objeto de los contactos
+export  const contactos = [
+    //objetos con la informacion del contacto
+    {   
+        'id':1,
+        'nombre':'Linkedin',
+        'icono':'fa-brands fa-linkedin-in',
+        'link':''
+    },
+
+    {
+        'id':2,
+        'nombre':'Instagram',
+        'icono':'fa-brands fa-instagram',
+        'link':''
+    }
+]
